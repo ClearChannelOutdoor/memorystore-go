0ed7f9b733c4a82ed717f53d2864c7768744d74d
Miguel Ruiz <miguelruiz@clearchannel.com>
2022-10-10 15:19:57 -0700

    write mock test

--- tests/memorystore_test.go
+++ tests/memorystore_test.go
@@ -1,20 +1,21 @@
 package memorystore_tests
 
 import (
-	"fmt"
+	// "fmt"
 	memorystore_go "github.com/clearchanneloutdoor/memorystore-go"
 	// "sync"
+	"encoding/json"
+	"github.com/go-redis/redismock/v8"
 	"github.com/stretchr/testify/assert"
 	"testing"
-	// "time"
-	"github.com/go-redis/redismock/v8"
+	"time"
 )
 
 type TestData struct {
 	Value string
 }
 
-func TestRedisMockWrite(t *testing.T) {
+func TestRedisMockGet(t *testing.T) {
 
 	//var d = TestData{
 	//	Value: "woosh",
@@ -26,11 +27,32 @@ func TestRedisMockWrite(t *testing.T) {
 
 	mock.ExpectGet("get-key").RedisNil()
 
-	value, err := client.Get("get-key")
-
-	fmt.Println(err)
+	value, _ := client.Get("get-key")
 
 	assert.Equal(t, value, "")
+	mock.ClearExpect()
 
 	// r.client.Ping(r.ctx).Err()
+}
+
+func TestRedisMockWrite(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+	client := memorystore_go.NewRedisMock(db)
+	valueBytes, encodingError := json.Marshal("set-value")
+	if encodingError != nil {
+		t.Error(encodingError)
+	}
+	mock.ExpectSet("set-key", valueBytes, 10*time.Minute)
+
+	client.Set("set-key", "set-value", 10*time.Minute)
+
+	//if err == nil || err.Error() != "FAIL" {
+	//	t.Error("Wrong Error Recieved")
+	//}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+
+	mock.ClearExpect()
 }
